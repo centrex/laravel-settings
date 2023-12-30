4900d1aef89aecda7c8cce7d202306f11e131eab
rochi88 <raisul.me@gmail.com>
2023-12-30 18:56:34 +0600

    clear setting config

--- config/config.php
+++ config/config.php
@@ -6,14 +6,5 @@ declare(strict_types = 1);
  * You can place your custom package configuration in here.
  */
 return [
-        /*
-    |--------------------------------------------------------------------------
-    | Settings Table
-    |--------------------------------------------------------------------------
-    |
-    | Database table used to store settings in.
-    |
-    */
-    'table' => 'settings',
 
 ];
